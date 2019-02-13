3d837823d84713951bf878051588f3229085975f
Elizabeth Kimita <kimita.wanjiku@gmail.com>
2019-02-13 11:01:17 +0300

    adding working temperature converter

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var temperature = function(celsius, number2) {
+ return celsius + number2;
+};
+
+var celsius = prompt ("Enter your temperature in degrees celsius here:");
+var degrees = celsius * (1.8);
+var number2 = 32;
+var results = degrees + number2;
+alert(results);
